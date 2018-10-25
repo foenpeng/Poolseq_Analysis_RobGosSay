a2ebf6372e33d4960edf562c7830838e352e0380
Foen Peng <foenpeng@uw.edu>
2018-10-25 15:39:44 -0400

    Combined Tajima's D with PBS data

--- Pooseq_Foen_RobGosSay.R
+++ Pooseq_Foen_RobGosSay.R
@@ -25,7 +25,6 @@
 #  1.  Load packages
 #################################################################
 library(data.table)
-#library(ape)
 
 #################################################################
 #  2.  Load data and organize as needed
@@ -81,7 +80,7 @@ setwd("/Users/pengfoen/Documents/Research/Bolnick lab/Analyses_Poolseq/Data")
   FstAll[GSFst.nonneg == 1.0, GSFst.nonneg := 0.99999]
   FstAll[RSFst.nonneg == 1.0, RSFst.nonneg := 0.99999]
 
-  # Set a copy number threshold as 20 the minimum and 150 the maximum.
+  # Set a copy number threshold as 20 for the minimum and 150 for the maximum.
   FstAll[,NreadsOK:=F]
   FstAll[GosNreads >19 & RobNreads >19 & SayNreads >19 & GosNreads <151 & RobNreads <151 & SayNreads <151,
          NreadsOK:= T]
@@ -98,15 +97,6 @@ setwd("/Users/pengfoen/Documents/Research/Bolnick lab/Analyses_Poolseq/Data")
   FstAll[substr(LG, 1, 2) == "gr", LGn := as.numeric(as.roman(substr(LG, 6, nchar(LG))))]
   FstAll[substr(LG, 1, 2) == "sc", LGn := as.numeric(substr(LG, 10, nchar(LG)))] 
   
-  #### Define a continuous genomic x axis, MTDNA near zero
-  Chrlengths <- read.table("Stickle_chr_lengths.txt", sep = ",")
-  chrmax <- FstAll[, .(max_length = max(Pos)), by = LGn]
-  chrmax[2:22,max_length:= as.vector(as.matrix(Chrlengths) )]
-  chrmax[,Cumulative_Chrlengths := cumsum(max_length)]
-  Cumulative_ChrSTART <- chrmax[,shift(Cumulative_Chrlengths,1,0,"lag")]
-  FstAll[, Cumulative_position := Pos + Cumulative_ChrSTART[FstAll[,LGn]]]
-  
-
 }
 
 ##############
@@ -117,30 +107,72 @@ windowsize <- 1000
 FstAll[,SNP_window:= (Pos%/%windowsize)*windowsize]
 temp_BinSNPs<-FstAll[NreadsOK==T,.("PBS.r" = mean(pbs.r), "nSNPs" = .N),by=.(LGn, SNP_window)]
 
-# Create a data.table with the full length of genome for BinSNPs to join in.
-Chrlengths <- read.table("Stickle_chr_lengths.txt", sep = ",")
+# Create a data.table with the full length of genome for temp_BinSNPs to join in.
+chrlengths <- read.table("Stickle_chr_lengths.txt", sep = ",")
 chr <- FstAll[, .(chr_length = max(Pos)), by = LGn]
 chr[2:22,chr_length:= as.vector(as.matrix(Chrlengths) )]
-chr[,Cumulative_Chrlengths := cumsum(chr_length)]
-chr[,Cumulative_ChrSTART := shift(Cumulative_Chrlengths,1,0,"lag")]
+chr[,cumulative_chrlengths := cumsum(chr_length)]
+chr[,cumulative_chrSTART := shift(cumulative_chrlengths,1,0,"lag")]
 chr_forjoin<-chr[rep(1:.N,chr_length%/%windowsize+1)][,window_pos := ((1:.N)-1)*windowsize, by = LGn]
 
+# join the smoothed Fst with the chr_forjoin, the latter has the full length of chromosome
 setkey(chr_forjoin, LGn, window_pos)
 setkey(temp_BinSNPs, LGn, SNP_window)
 
 BinSNPs <- temp_BinSNPs[chr_forjoin]
-BinSNPs[,Cumulative_window_pos:=SNP_window+Cumulative_ChrSTART]
-BinSNPs <- BinSNPs[,!c("chr_length","Cumulative_Chrlengths","Cumulative_ChrSTART")]
+BinSNPs[,cumulative_window_pos:=SNP_window+cumulative_chrSTART]
+BinSNPs <- BinSNPs[,!c("chr_length","cumulative_chrlengths","cumulative_chrSTART")]
 BinSNPs[is.na(nSNPs),nSNPs:=0]
 BinSNPs[is.na(PBS.r),PBS.r:=0]
+BinSNPs[,Pos := SNP_window + 1000]
 
 ######################
 #  Load Tajima's D 
-setwd("./TajD/subsample50x_mincount2_finished/")
-TajD_filelist = list.files( pattern="group.*Rob.Q15") 
-TajD.r <- do.call(rbind,lapply(TajD_filelist,function(i){fread(i, , sep = "\t", header = F)}))
-colnames(TajD.r) <- c("LG", "Pos", "Nsnp.r", "x.r", "D.r")
+TajD_r_filelist = list.files(path = "./TajD/subsample50x_mincount2_finished", pattern="Rob.Q15", full.names = T) 
+TajD_r <- do.call(rbind,lapply(TajD_r_filelist,function(i){fread(i, sep = "\t", header = F)}))
+colnames(TajD_r) <- c("LG", "Pos", "Nsnp.r", "x.r", "D.r")
 
-TajD.r[substr(LG, 1, 2) == "MT", LGn := 0]
-TajD.r[substr(LG, 1, 2) == "gr", LGn := as.numeric(as.roman(substr(LG, 6, nchar(LG))))]
-TajD,r[substr(LG, 1, 2) == "sc", LGn := as.numeric(substr(LG, 10, nchar(LG)))] 
+TajD_r[substr(LG, 1, 2) == "MT", LGn := 0]
+TajD_r[substr(LG, 1, 2) == "gr", LGn := as.numeric(as.roman(substr(LG, 6, nchar(LG))))]
+TajD_r[substr(LG, 1, 2) == "sc", LGn := as.numeric(substr(LG, 10, nchar(LG)))]
+TajD_r<-TajD_r[,D.r := as.numeric(D.r)]
+TajD_r<-TajD_r[!is.na(D.r)]
+setorder(TajD_r, LGn,Pos)
+
+setkey(TajD_r, LGn, Pos)
+setkey(BinSNPs, LGn, Pos)
+
+TajD_PBS <- TajD_r[BinSNPs]
+TajD_PBS_cleaned <- TajD_PBS[,.(LGn,Pos,cumulative_window_pos,"D_nSNPs" = Nsnp.r,D.r ,"PBS_nSNPs" = nSNPs, PBS.r)]
+
+####
+Pi_r_filelist = list.files(path = "./per_chromosom_pi", pattern="Rob.Q15", full.names = T) 
+Pi_r <- do.call(rbind,lapply(Pi_r_filelist,function(i){fread(i, sep = "\t", header = F)}))
+colnames(Pi_r) <- c("LG", "Pos", "Pi_nSNPs", "x.r", "Pi.r")
+
+Pi_r[substr(LG, 1, 2) == "MT", LGn := 0]
+Pi_r[substr(LG, 1, 2) == "gr", LGn := as.numeric(as.roman(substr(LG, 6, nchar(LG))))]
+Pi_r[substr(LG, 1, 2) == "sc", LGn := as.numeric(substr(LG, 10, nchar(LG)))]
+Pi_r<-Pi_r[,Pi.r := as.numeric(Pi.r)]
+Pi_r<-Pi_r[!is.na(Pi.r)]
+setorder(Pi_r, LGn,Pos)
+
+### to convert 2500 to 1000
+
+setkey(Pi_r, LGn, Pos)
+setkey(BinSNPs, LGn, Pos)
+
+TajD_PBS <- Pi_r[BinSNPs]
+TajD_PBS_cleaned <- TajD_PBS[,.(LGn,Pos,cumulative_window_pos,"D_nSNPs" = Nsnp.r,D.r ,"PBS_nSNPs" = nSNPs, PBS.r)]
+
+
+
+
+
+
+
+
+TajD_PBS_cleaned[LGn == 1,]
+plot(x = TajD_PBS_cleaned[,cumulative_window_pos], y = TajD_PBS_cleaned[,c(PBS.r)])
+par(new = T)
+plot(x = TajD_PBS_cleaned[,cumulative_window_pos], y = TajD_PBS_cleaned[,c(D.r)])
