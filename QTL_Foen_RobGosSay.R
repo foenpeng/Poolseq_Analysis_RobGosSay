f7fdb36b5a9abb15cb053aba03eddf8b4e73836c
PengFoen <peng.foen@gmail.com>
2019-02-03 08:54:09 -0800

    when joining snps data with focal region, keep all snp marker information, as it is easier to plot.
    Also update the plot function to plot all traits with a for loop

--- QTL_Foen_RobGosSay.R
+++ QTL_Foen_RobGosSay.R
@@ -60,37 +60,35 @@
   
   # join the focal region with the snp data
   qtl_all[,Pos.join:=Pos]
-  qtl_all_focal_snp<-qtl_all[qtl_all_focal_region, 
-          on = c("Pos.join>start","Pos.join<end","LGn","qtl.trait"),
-          nomatch = 0L]
-  
-  # add the few significant scaffolds to the joined snp data
-  qtl_sig_scaffolds<-qtl_all[substr(V1, 1, 2) == "sc" & sig == TRUE,]
-  qtl_all_focal_snp<- merge(qtl_sig_scaffolds,qtl_all_focal_snp,all=TRUE)
-  
+  qtl_all_focal_region[,c("qtl.focal.region.start","qtl.focal.region.end"):=.(start,end)]
+  qtl_all_focal_snp<-qtl_all_focal_region[qtl_all, 
+          on = c("start<Pos.join","end>Pos.join","LGn","qtl.trait"),
+          nomatch = NA]
+
   # some data clearning and saving the file
-  setnames(qtl_all_focal_snp,c("Pos.join","Pos.join.1","width"), c("qtl.focal.region.start","qtl.focal.region.end","qtl.focal.region.width"))
-  qtl_all_focal_snp[,c("focal.qtl.start","focal.qtl.end"):=NULL]
+  setnames(qtl_all_focal_snp,"width", "qtl.focal.region.width")
+  qtl_all_focal_snp[,c("focal.qtl.start","focal.qtl.end","start","end"):=NULL]
   fwrite(qtl_all_focal_snp,"./result/Foen/QTL_AllTraits_snp_in_focal_region.csv")
 }  
 
 # plot qtl for each trait
 {
-  for(i in qtl_all_focal_snp[,unqiue(qtl.trait)]){
-    qtl_plot<-qtl_all_focal_snp[qtl.trait==i,]
+  for(i in qtl_all_focal_snp[,unique(qtl.trait)]){
+    qtl_plot<-subset(qtl_all_focal_snp, qtl.trait==i)
     
     png(sprintf("./result/Foen/%s_QTL.png",i), width = 4000, height = 2000,res=300)
     setkey(qtl_plot,LGn,Pos)
     chrcol <- 2+2*as.numeric(qtl_plot[,LGn] %%2 == 1)
     chrpch <- ifelse(qtl_plot[,sig], 16, 1)
-    plot(qtl_plot[,Cumulative_position],qtl_plot[,abs_Z], col=chrcol, type = 'o', axes=F, xlab = "", ylab = "Worm Presence Z_SNP",pch=chrpch, ylim = c(0, 5))
+    plot(qtl_plot[,Cumulative_position],qtl_plot[,abs_Z], col=chrcol, type = 'o', axes=F, xlab = "Linkage Group", ylab = sprintf("%s Z_SNP",i),pch=chrpch, ylim = c(0, 6))
     text(qtl_plot[sig==T,Cumulative_position],qtl_plot[sig==T,abs_Z], labels = qtl_plot[sig==T,snp.id],cex=0.7, pos=2)
     abline(h=qtl_plot[sig==FALSE, max(abs_Z)], lty=5, col='black')
     Chr.mid <- Cumulative_ChrSTART + Chrlengths / 2
     axis(2)
     axis(1, at= Chr.mid, labels = 1:21)
-    rect(Cumulative_ChrSTART,0,Cumulative_Chrlengths,5,border = rgb(0,0,0,0.5), lwd = 2)
-    rect(qtl_plot_focal[,cum.start], 0,  qtl_plot_focal[,cum.end],5, border = rgb(1,0,0,0.2) , lwd = 2, col = rgb(1,0,0,0.2))
+    rect(Cumulative_ChrSTART,0,Cumulative_Chrlengths,6,border = rgb(0,0,0,0.5), lwd = 2)
+    focal_region_plot<-na.omit(qtl_plot[,.(unique(qtl.focal.region.cum.start),unique(qtl.focal.region.cum.end))])
+    rect(focal_region_plot[,V1], 0,  focal_region_plot[,V2],6, border = rgb(1,0,0,0.2) , lwd = 2, col = rgb(1,0,0,0.2))
     dev.off()
     
   }
